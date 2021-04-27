90bac8899de81b088a24d81540883d963c4e507c
PrayinForRain <46566891+prayinforrain@users.noreply.github.com>
2021-04-27 14:50:43 +0900

    react init
    a

---
+++ musicstore/src/Mainpage.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ musicstore/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div class="footer_container">
+                <div class="names">오윤환 이우재 전윤철</div>
+                <div class="git_address">
+                    <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://github.com/prayinforrain/2021_SoftwareEngineering"
+                    >
+                        https://github.com/prayinforrain/2021_SoftwareEngineering
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
